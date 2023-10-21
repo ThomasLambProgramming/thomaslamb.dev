f7b1f9ed7764902c2cea9a1a23c7fb8f03c358ec
Thomas Lamb <thomaslambprogramming@gmail.com>
2023-10-21 17:28:25 +1100

    This hover effect is very annoying

--- src/Components/ProjectContainer.tsx
+++ src/Components/ProjectContainer.tsx
@@ -9,7 +9,7 @@ const ProjectContainer: React.FC<{projects: string[], typeTitle: string}> = ({pr
         <h2>
           {typeTitle}
         </h2>
-        <div className='grid grid-cols-2 md:grid-cols-3 gap-4 w-auto m-auto min-w-[1500px] max-w-screen-xl justify-center object-fill'>
+        <div className='grid grid-cols-2 md:grid-cols-3 h-auto gap-4 w-auto m-auto max-w-screen-xl justify-center object-fill'>
         {projects.map((project: string) => (
           <li className='list-none'>{<ProjectPreview projectName={project} />}</li>
           ))}
--- src/Components/ProjectPreview.tsx
+++ src/Components/ProjectPreview.tsx
@@ -2,54 +2,45 @@ import React from 'react';
 
 const ProjectPreview: React.FC<{projectName: string}> = ({projectName}) =>
 {
-    return (
-      // <div>
-      //   <div>
-      //     <img
-      //       className="rounded-lg h-auto max-w-full"
-      //       src="ProjectAssets\Complex\ComplexExample.gif"
-      //       alt=""
-      //     />
-      //   </div>
-      //   <a href="">
-      //     <h2>{projectName}</h2>
-      //     <div className="content">
-      //       <p>
-      //         I created a node generation system to take in an environment mesh
-      //         and produce a node graph for Ai agents.
-      //       </p>
-      //     </div>
-      //   </a>
-      // </div>
+  return (
+    <div className="flex flex-col items-center justify-center mt-12">
+      <a className="relative block bg-slate-900 group" href="##">
+        <img
+          className="relative inset-0 object-cover w-full h-full group-hover:opacity-50 "
+          src="ProjectAssets\Complex\ComplexExample.gif"/>
 
-      <div className="flex items-center justify-center mt-12">
-        <a className="relative block w-full bg-slate-900 group" href="##">
-          <img
-            className="absolute inset-0 object-fill 
-                                w-full h-full group-hover:opacity-50 "
-            src="ProjectAssets\Complex\ComplexExample.gif"
-          />
-          <div className="relative align-top text-center">
-            <div className="mt-40">
-              {/* Hidden content */}
-              <div className="transition-all transform 
-                                opacity-0
-                                group-hover:opacity-100 
-                                group-hover:-translate-y-1/2">
-                <h3 className="text-3xl text-white mb-4">Project Title</h3>
-                <p className="text-lg text-white mr-4 ml-4">
-                  {/* Put in text content here */}
-                  Default Text Content TestDefault Text Content TestDefault Text
-                  Content TestDefault Text Content TestDefault Text Content
-                  TestDefault Text Content Test
-                </p>
-              </div>
-              {/* End of hidden content */}
-            </div>
+          <div className="transition-all flex-1 text-center absolute opacity-0 group-hover:-translate-y-[200%] group-hover:opacity-100 duration-300">
+            <h3 className="text-3xl flex-1 text-white mb-4 opacity-100">
+              {projectName}  
+            </h3>
+            <p className="text-lg flex-1 text-white mr-4 ml-4">
+              {/* Put in text content here */}
+              This is some default text to represent what would be a regular discription of a project
+              This is so i can get some sizing correct.
+            </p>
           </div>
-        </a>
-      </div>
-    );
+      </a>
+    </div>
+  );
 }
 
 export default ProjectPreview;
+
+// <div>
+//   <div>
+//     <img
+//       className="rounded-lg h-auto max-w-full"
+//       src="ProjectAssets\Complex\ComplexExample.gif"
+//       alt=""
+//     />
+//   </div>
+//   <a href="">
+//     <h2>{projectName}</h2>
+//     <div className="content">
+//       <p>
+//         I created a node generation system to take in an environment mesh
+//         and produce a node graph for Ai agents.
+//       </p>
+//     </div>
+//   </a>
+// </div>
