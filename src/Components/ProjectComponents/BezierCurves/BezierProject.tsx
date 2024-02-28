c27239529aeb62c00947083a166a623121366503
Thomas Lamb <thomaslambprogramming@gmail.com>
2024-02-28 11:02:34 +1100

    Fixed fragment import issue so should build now

--- src/Components/ProjectComponents/BezierCurves/BezierProject.tsx
+++ src/Components/ProjectComponents/BezierCurves/BezierProject.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ProjectTopicPoint from '../ProjectTopicPoint';
 
 const BezierProject: React.FC<{ darkModeEnabled: boolean }> = ({ darkModeEnabled }) => {
