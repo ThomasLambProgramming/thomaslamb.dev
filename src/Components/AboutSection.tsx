3a4e4159421407cf205f3fe1e0079b86371e2206
Thomas Lamb <thomaslambprogramming@gmail.com>
2024-02-28 15:04:18 +1100

    Added about section change

--- src/Components/AboutSection.tsx
+++ src/Components/AboutSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AboutSection: React.FC<{ isDarkMode: boolean, defaultText: string[] }> = ({ isDarkMode, defaultText }) => {
   return (
-    <div className={"" + (isDarkMode ? "shadow-gray-500 shadow-sm" : "")}>
+    <div className={"" + (isDarkMode ? "shadow-gray-700 shadow-md" : "")}>
       <h1 className='text-xl dark:text-Neutral-0 text-DarkNeutralN-100 p-2'>About me</h1>
       {defaultText.map((textValue: string) => (
         <p className='p-2' key={textValue.substring(0, textValue.length > 10 ? 10 : textValue.length)}>{textValue}</p>
