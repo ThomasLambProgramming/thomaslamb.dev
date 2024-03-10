7436efaabdb67dcfe3e8db1a26b6ef336af4d183
Thomas Lamb <thomaslambprogramming@gmail.com>
2024-03-11 03:21:51 +1100

    I was looking at an old vercel failure point, blankproject.tsx just had an unused variable

--- src/Components/ProjectComponents/DefaultBlank/DefaultBlankProject.tsx
+++ src/Components/ProjectComponents/DefaultBlank/DefaultBlankProject.tsx
@@ -1,12 +1,11 @@
 import React, { Fragment } from 'react';
 
-const DefaultBlankProject: React.FC<{darkModeEnabled: boolean}> = ({darkModeEnabled}) =>
-{
-    return (
+const DefaultBlankProject: React.FC<{ darkModeEnabled: boolean }> = ({ darkModeEnabled }) => {
+  return (
     <Fragment>
-
+      <p className={`${darkModeEnabled ? "" : ""}`}></p>
     </Fragment>
-    );
+  );
 }
 
 export default DefaultBlankProject;
