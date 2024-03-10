7cb1f70d9475c281855c21d349e391ec24eac2e3
Thomas Lamb <thomaslambprogramming@gmail.com>
2024-03-11 03:17:59 +1100

    Hopefully fixed npm errors

--- src/Components/ProjectComponents/NodeGraphGenerator/NodeGraphGeneratorProject.tsx
+++ src/Components/ProjectComponents/NodeGraphGenerator/NodeGraphGeneratorProject.tsx
@@ -11,7 +11,7 @@ const NodeGraphGeneratorProject: React.FC<{ darkModeEnabled: boolean }> = ({ dar
       <ProjectTopicPoint isDarkMode={darkModeEnabled}
         projectPointTexts={["For my complex games systems assessment, I wanted to work with AI, but found unity's AI system hard to operate with custom locomotion.", "I then decided to create a simple to use node graph generator. This generator would take an object as an environment and use it to create a node graph on top of it."]}
         projectImgSrc='/ProjectAssets/NodeGraphGenerator/ComplexExample.gif'
-      />
+      ></ProjectTopicPoint>
 
       <img src="/ProjectAssets/NodeGraphGenerator/ComplexOne.png"></img>
 
@@ -19,14 +19,14 @@ const NodeGraphGeneratorProject: React.FC<{ darkModeEnabled: boolean }> = ({ dar
         isDarkMode={darkModeEnabled}
         projectPointTexts={[`One of the first problems that I ran into with this system was filtering out the vertices of objects that were not needed.`, `The solution to this was a dot product check against that object's other vertices to make sure that they were not underneath another vertex within a set distance.`, `If they were then they are ignored. After this is done on all objects in the environment they are then collected into a single list container where they are filtered by distance, and heights to remove overlaps, placed into a node class and given connections then all the node objects are then collected into a single array to be used as a node graph.`]}
         projectImgSrc='/ProjectAssets/NodeGraphGenerator/ComplexFiltering.png'
-      />
+      ></ProjectTopicPoint>
 
 
       <ProjectTopicPoint
         isDarkMode={darkModeEnabled}
         projectPointTexts={["To make it simple to use I made the system require a single object", "this could be an empty child that has multiple hundreds of objects; the reasoning for this was so all objects are accounted for under the one environment object, so all objects with a mesh are processed without individually referencing them."]}
         projectImgSrc='/ProjectAssets/NodeGraphGenerator/ComplexAssessment.gif'
-      />
+      ></ProjectTopicPoint>
 
 
       <div className='flex flex-col w-[70%] bg-DarkNeutral-200 p-10'>
